1c5cf299208b72f767664f6dc0ee812882ac451b
XLiang <yunlonliang@gmail.com>
2022-03-10 16:08:37 +0800

    Add files via upload

---
+++ src_code/fine-thumt-con-new/thumt/models/transformer.py
@@ -0,0 +1,495 @@
+# coding=utf-8
+# Copyright 2017-2019 The THUMT Authors
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import copy
+import code
+import tensorflow as tf
+import thumt.layers as layers
+import thumt.losses as losses
+import thumt.utils as utils
+import numpy as np
+
+from thumt.models.model import NMTModel
+NUMBER=142871
+RCNN_FEATURE_DIM = 2048
+MAX_OBJECTS = 20
+
+def _load_object_embedding(mode, params, uniform_scale = 0.25, dimension_size = 300, embed_file='glove'):
+    n = 0
+    path = "/path/to/object_features/all.objects.mmap"
+    word_vectors = np.memmap(path, mode="r", dtype=np.float32, shape=(NUMBER, MAX_OBJECTS, RCNN_FEATURE_DIM))
+
+    return np.array(word_vectors, dtype=np.float32)
+
+def _load_mask_embedding(mode, params, uniform_scale = 0.25, dimension_size = 300, embed_file='glove'):
+    n = NUMBER
+    path = "/path/to/multimodal_mt/object_features/all.objects_mask.mmap"
+    word_vectors = np.memmap(path, mode="r", dtype=np.bool, shape=(NUMBER, MAX_OBJECTS))
+
+    return np.array(word_vectors, dtype=np.float32)
+
+def _layer_process(x, mode):
+    if not mode or mode == "none":
+        return x
+    elif mode == "layer_norm":
+        return layers.nn.layer_norm(x)
+    else:
+        raise ValueError("Unknown mode %s" % mode)
+
+
+def _residual_fn(x, y, keep_prob=None):
+    if keep_prob and keep_prob < 1.0:
+        y = tf.nn.dropout(y, keep_prob)
+    return x + y
+
+
+def _ffn_layer(inputs, hidden_size, output_size, keep_prob=None,
+               dtype=None, scope=None):
+    with tf.variable_scope(scope, default_name="ffn_layer", values=[inputs],
+                           dtype=dtype):
+        with tf.variable_scope("input_layer"):
+            hidden = layers.nn.linear(inputs, hidden_size, True, True)
+            hidden = tf.nn.relu(hidden)
+
+        if keep_prob and keep_prob < 1.0:
+            hidden = tf.nn.dropout(hidden, keep_prob)
+
+        with tf.variable_scope("output_layer"):
+            output = layers.nn.linear(hidden, output_size, True, True)
+
+        return output
+
+
+def transformer_encoder(inputs, bias, params, dtype=None, scope=None):
+    with tf.variable_scope(scope, default_name="encoder", dtype=dtype,
+                           values=[inputs, bias]):
+        x = inputs
+        for layer in range(params.num_encoder_layers):
+            with tf.variable_scope("layer_%d" % layer):
+                with tf.variable_scope("self_attention"):
+                    max_relative_dis = params.max_relative_dis \
+                        if params.position_info_type == 'relative' else None
+
+                    y = layers.attention.multihead_attention(
+                        _layer_process(x, params.layer_preprocess),
+                        None,
+                        bias,
+                        params.num_heads,
+                        params.attention_key_channels or params.hidden_size,
+                        params.attention_value_channels or params.hidden_size,
+                        params.hidden_size,
+                        1.0 - params.attention_dropout,
+                        max_relative_dis=max_relative_dis,
+                    )
+                    y = y["outputs"]
+                    x = _residual_fn(x, y, 1.0 - params.residual_dropout)
+                    x = _layer_process(x, params.layer_postprocess)
+
+                with tf.variable_scope("feed_forward"):
+                    y = _ffn_layer(
+                        _layer_process(x, params.layer_preprocess),
+                        params.filter_size,
+                        params.hidden_size,
+                        1.0 - params.relu_dropout,
+                    )
+                    x = _residual_fn(x, y, 1.0 - params.residual_dropout)
+                    x = _layer_process(x, params.layer_postprocess)
+
+        outputs = _layer_process(x, params.layer_preprocess)
+
+        return outputs
+
+
+def transformer_decoder(inputs, memory, bias, mem_bias, params, state=None,
+                        dtype=None, scope=None):
+    with tf.variable_scope(scope, default_name="decoder", dtype=dtype,
+                           values=[inputs, memory, bias, mem_bias]):
+        x = inputs
+        next_state = {}
+        for layer in range(params.num_decoder_layers):
+            layer_name = "layer_%d" % layer
+            with tf.variable_scope(layer_name):
+                layer_state = state[layer_name] if state is not None else None
+                max_relative_dis = params.max_relative_dis \
+                        if params.position_info_type == 'relative' else None
+
+                with tf.variable_scope("self_attention"):
+                    y = layers.attention.multihead_attention(
+                        _layer_process(x, params.layer_preprocess),
+                        None,
+                        bias,
+                        params.num_heads,
+                        params.attention_key_channels or params.hidden_size,
+                        params.attention_value_channels or params.hidden_size,
+                        params.hidden_size,
+                        1.0 - params.attention_dropout,
+                        state=layer_state,
+                        max_relative_dis=max_relative_dis,
+                    )
+
+                    if layer_state is not None:
+                        next_state[layer_name] = y["state"]
+
+                    y = y["outputs"]
+                    x = _residual_fn(x, y, 1.0 - params.residual_dropout)
+                    x = _layer_process(x, params.layer_postprocess)
+
+                with tf.variable_scope("encdec_attention"):
+                    y = layers.attention.multihead_attention(
+                        _layer_process(x, params.layer_preprocess),
+                        memory,
+                        mem_bias,
+                        params.num_heads,
+                        params.attention_key_channels or params.hidden_size,
+                        params.attention_value_channels or params.hidden_size,
+                        params.hidden_size,
+                        1.0 - params.attention_dropout,
+                        max_relative_dis=max_relative_dis,
+                    )
+                    y = y["outputs"]
+                    x = _residual_fn(x, y, 1.0 - params.residual_dropout)
+                    x = _layer_process(x, params.layer_postprocess)
+
+                with tf.variable_scope("feed_forward"):
+                    y = _ffn_layer(
+                        _layer_process(x, params.layer_preprocess),
+                        params.filter_size,
+                        params.hidden_size,
+                        1.0 - params.relu_dropout,
+                    )
+                    x = _residual_fn(x, y, 1.0 - params.residual_dropout)
+                    x = _layer_process(x, params.layer_postprocess)
+
+        outputs = _layer_process(x, params.layer_preprocess)
+
+        if state is not None:
+            return outputs, next_state
+
+        return outputs
+
+
+def encoding_graph(features, mode, params):
+    if mode != "train":
+        params.residual_dropout = 0.0
+        params.attention_dropout = 0.0
+        params.relu_dropout = 0.0
+        params.label_smoothing = 0.0
+
+    dtype = tf.get_variable_scope().dtype
+    hidden_size = params.hidden_size
+    src_seq = features["source"]
+    src_len = features["source_length"]
+    src_mask = tf.sequence_mask(src_len,
+                                maxlen=tf.shape(features["source"])[1],
+                                dtype=dtype or tf.float32)
+    svocab = params.vocabulary["source"]
+    src_vocab_size = len(svocab)
+    initializer = tf.random_normal_initializer(0.0, params.hidden_size ** -0.5)
+
+    if params.shared_source_target_embedding:
+        src_embedding = tf.get_variable("weights",
+                                        [src_vocab_size, hidden_size],
+                                        initializer=initializer)
+    else:
+        src_embedding = tf.get_variable("source_embedding",
+                                        [src_vocab_size, hidden_size],
+                                        initializer=initializer)
+
+    bias = tf.get_variable("bias", [hidden_size])
+
+    inputs = tf.gather(src_embedding, src_seq)
+
+    if params.multiply_embedding_mode == "sqrt_depth":
+        inputs = inputs * (hidden_size ** 0.5)
+
+    inputs = inputs * tf.expand_dims(src_mask, -1)
+    #code.interact(local=locals())
+    encoder_input = tf.nn.bias_add(inputs, bias)
+
+    obj_input = tf.layers.dense(features["image"], hidden_size, activation=tf.nn.tanh)
+    obj_mask_input = features["image_mask"]
+    #code.interact(local=locals())
+    obj_mask = tf.expand_dims(obj_mask_input, -1)
+    obj_input = obj_input * obj_mask
+
+    print("image_input", obj_input, obj_input)
+    ####multimodal position embedding
+    text_pos = tf.ones_like(src_mask, dtype=tf.int32)
+    image_pos = tf.zeros_like(obj_mask_input, dtype=tf.int32)
+    multimodal_pos = tf.concat([text_pos, image_pos], -1)
+    
+    with tf.variable_scope("multimodal_pos_embedding", reuse=tf.AUTO_REUSE):
+        tv_pos_emb = tf.get_variable("multimodal_pos_embedding", [2, hidden_size], initializer=tf.contrib.layers.xavier_initializer()) 
+    #multimodal = tf.concat([encoder_input, obj_input * obj_mask_input], -2)
+    multimodal_pos =  tf.nn.embedding_lookup(tv_pos_emb, multimodal_pos)
+    #encoder_input = tf.layers.dense(multimodal, hidden_size, activation=tf.nn.tanh)
+    encoder_input = tf.concat([encoder_input, obj_input], -2)
+    encoder_input += multimodal_pos
+    
+#    code.interact(local=locals())
+    com_mask = tf.concat([src_mask, obj_mask_input], -1)
+    enc_attn_bias = layers.attention.attention_bias(com_mask, "masking",
+                                                    dtype=dtype)
+    print("enc_attn_bias", enc_attn_bias)
+    if params.position_info_type == 'absolute':
+        encoder_input = layers.attention.add_timing_signal(encoder_input)
+
+    if params.residual_dropout:
+        keep_prob = 1.0 - params.residual_dropout
+        encoder_input = tf.nn.dropout(encoder_input, keep_prob)
+
+    encoder_output = transformer_encoder(encoder_input, enc_attn_bias, params)
+
+    return encoder_output, enc_attn_bias
+
+
+def decoding_graph(features, state, mode, params):
+    if mode != "train":
+        params.residual_dropout = 0.0
+        params.attention_dropout = 0.0
+        params.relu_dropout = 0.0
+        params.label_smoothing = 0.0
+
+    dtype = tf.get_variable_scope().dtype
+    tgt_seq = features["target"]
+    src_len = features["source_length"]
+    tgt_len = features["target_length"]
+    src_mask = tf.sequence_mask(src_len,
+                                maxlen=tf.shape(features["source"])[1],
+                                dtype=dtype or tf.float32)
+    tgt_mask = tf.sequence_mask(tgt_len,
+                                maxlen=tf.shape(features["target"])[1],
+                                dtype=dtype or tf.float32)
+
+    hidden_size = params.hidden_size
+    tvocab = params.vocabulary["target"]
+    tgt_vocab_size = len(tvocab)
+    svocab = params.vocabulary["source"]
+    src_vocab_size = len(svocab)
+    initializer = tf.random_normal_initializer(0.0, params.hidden_size ** -0.5)
+
+    if params.shared_source_target_embedding:
+        with tf.variable_scope(tf.get_variable_scope(), reuse=True):
+            tgt_embedding = tf.get_variable("weights",
+                                            [src_vocab_size, hidden_size],
+                                            initializer=initializer)
+    else:
+        tgt_embedding = tf.get_variable("target_embedding",
+                                        [tgt_vocab_size, hidden_size],
+                                        initializer=initializer)
+
+    if params.shared_embedding_and_softmax_weights:
+        weights = tgt_embedding
+    else:
+        weights = tf.get_variable("softmax", [tgt_vocab_size, hidden_size],
+                                  initializer=initializer)
+
+    targets = tf.gather(tgt_embedding, tgt_seq)
+
+    if params.multiply_embedding_mode == "sqrt_depth":
+        targets = targets * (hidden_size ** 0.5)
+
+    targets = targets * tf.expand_dims(tgt_mask, -1)
+    '''
+    with tf.variable_scope("img_embedding", reuse=tf.AUTO_REUSE):
+        mask_initializer = tf.constant_initializer(_load_mask_embedding(mode, params))
+        mask_emb = tf.get_variable(name='mask_weights', shape=(NUMBER, MAX_OBJECTS), initializer=mask_initializer, trainable=False)
+    obj_mask_input = tf.nn.embedding_lookup(mask_emb, features["image"])
+
+    com_mask = tf.concat([src_mask, obj_mask_input], -1)
+    enc_attn_bias = layers.attention.attention_bias(com_mask, "masking",
+                                                    dtype=dtype)
+    '''
+    enc_attn_bias = state["enc_attn_bias"]
+    dec_attn_bias = layers.attention.attention_bias(tf.shape(targets)[1],
+                                                    "causal", dtype=dtype)
+    # Shift left
+    decoder_input = tf.pad(targets, [[0, 0], [1, 0], [0, 0]])[:, :-1, :]
+    if params.position_info_type == 'absolute':
+        decoder_input = layers.attention.add_timing_signal(decoder_input)
+
+    if params.residual_dropout:
+        keep_prob = 1.0 - params.residual_dropout
+        decoder_input = tf.nn.dropout(decoder_input, keep_prob)
+
+    encoder_output = state["encoder"]
+
+    if mode != "infer":
+        decoder_output = transformer_decoder(decoder_input, encoder_output,
+                                             dec_attn_bias, enc_attn_bias,
+                                             params)
+    else:
+        decoder_input = decoder_input[:, -1:, :]
+        dec_attn_bias = dec_attn_bias[:, :, -1:, :]
+        decoder_outputs = transformer_decoder(decoder_input, encoder_output,
+                                              dec_attn_bias, enc_attn_bias,
+                                              params, state=state["decoder"])
+
+        decoder_output, decoder_state = decoder_outputs
+        decoder_output = decoder_output[:, -1, :]
+        logits = tf.matmul(decoder_output, weights, False, True)
+        log_prob = tf.nn.log_softmax(logits)
+
+        return log_prob, {"encoder": encoder_output, "decoder": decoder_state, "enc_attn_bias": enc_attn_bias}
+
+    decoder_output = tf.reshape(decoder_output, [-1, hidden_size])
+    logits = tf.matmul(decoder_output, weights, False, True)
+    labels = features["target"]
+
+    # label smoothing
+    ce = losses.smoothed_softmax_cross_entropy_with_logits(
+        logits=logits,
+        labels=labels,
+        smoothing=params.label_smoothing,
+        normalize=True
+    )
+    tgt_mask = tf.cast(tgt_mask, ce.dtype)
+
+    ce = tf.reshape(ce, tf.shape(tgt_seq))
+
+    if mode == "eval":
+        return -tf.reduce_sum(ce * tgt_mask, axis=1)
+
+    loss = tf.reduce_sum(ce * tgt_mask) / tf.reduce_sum(tgt_mask)
+
+    return loss
+
+
+def model_graph(features, mode, params):
+    encoder_output, enc_attn_bias = encoding_graph(features, mode, params)
+    state = {
+        "encoder": encoder_output,
+        "enc_attn_bias": enc_attn_bias
+    }
+    output = decoding_graph(features, state, mode, params)
+
+    return output
+
+
+class Transformer(NMTModel):
+
+    def __init__(self, params, scope="transformer"):
+        super(Transformer, self).__init__(params=params, scope=scope)
+
+    def get_training_func(self, initializer, regularizer=None, dtype=None):
+        def training_fn(features, params=None, reuse=None):
+            if params is None:
+                params = copy.copy(self.parameters)
+            else:
+                params = copy.copy(params)
+
+            custom_getter = utils.custom_getter if dtype else None
+
+            with tf.variable_scope(self._scope, initializer=initializer,
+                                   regularizer=regularizer, reuse=reuse,
+                                   custom_getter=custom_getter, dtype=dtype):
+                loss = model_graph(features, "train", params)
+                return loss
+
+        return training_fn
+
+    def get_evaluation_func(self):
+        def evaluation_fn(features, params=None):
+            if params is None:
+                params = copy.copy(self.parameters)
+            else:
+                params = copy.copy(params)
+
+            with tf.variable_scope(self._scope):
+                score = model_graph(features, "eval", params)
+
+            return score
+
+        return evaluation_fn
+
+    def get_inference_func(self):
+        def encoding_fn(features, params=None):
+            if params is None:
+                params = copy.copy(self.parameters)
+            else:
+                params = copy.copy(params)
+
+            with tf.variable_scope(self._scope):
+                encoder_output, enc_attn_bias = encoding_graph(features, "infer", params)
+                batch = tf.shape(encoder_output)[0]
+
+                state = {
+                    "encoder": encoder_output,
+                    "enc_attn_bias": enc_attn_bias,
+                    "decoder": {
+                        "layer_%d" % i: {
+                            "key": tf.zeros([batch, 0, params.attention_key_channels or params.hidden_size]),
+                            "value": tf.zeros([batch, 0, params.attention_value_channels or params.hidden_size])
+                        }
+                        for i in range(params.num_decoder_layers)
+                    }
+                }
+            return state
+
+        def decoding_fn(features, state, params=None):
+            if params is None:
+                params = copy.copy(self.parameters)
+            else:
+                params = copy.copy(params)
+
+            with tf.variable_scope(self._scope):
+                log_prob, new_state = decoding_graph(features, state, "infer",
+                                                     params)
+
+            return log_prob, new_state
+
+        return encoding_fn, decoding_fn
+
+    @staticmethod
+    def get_name():
+        return "transformer"
+
+    @staticmethod
+    def get_parameters():
+        params = tf.contrib.training.HParams(
+            pad="<pad>",
+            bos="<eos>",
+            eos="<eos>",
+            unk="<unk>",
+            append_eos=False,
+            hidden_size=512,
+            filter_size=2048,
+            num_heads=8,
+            num_encoder_layers=6,
+            num_decoder_layers=6,
+            attention_dropout=0.0,
+            residual_dropout=0.5,
+            relu_dropout=0.0,
+            label_smoothing=0.1,
+            attention_key_channels=0,
+            attention_value_channels=0,
+            layer_preprocess="none",
+            layer_postprocess="layer_norm",
+            multiply_embedding_mode="sqrt_depth",
+            shared_embedding_and_softmax_weights=False,
+            shared_source_target_embedding=False,
+            # Override default parameters
+            dev_object_mask="object_file_path",
+            dev_object="object_file_path",
+            image_num=10,
+            learning_rate_decay="linear_warmup_rsqrt_decay",
+            initializer="uniform_unit_scaling",
+            initializer_gain=1.0,
+            learning_rate=1.0,
+            batch_size=4096,
+            constant_batch_size=False,
+            adam_beta1=0.9,
+            adam_beta2=0.98,
+            adam_epsilon=1e-9,
+            clip_grad_norm=0.0,
+            # "absolute" or "relative"
+            position_info_type="relative",
+            # 8 for big model, 16 for base model, see (Shaw et al., 2018)
+            max_relative_dis=16
+        )
+
+        return params
